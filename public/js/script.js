3347bd7ad08e8b0df3c9fb88526f6689275899d2
usmanaqadri <usmanaqadri@gmail.com>
2022-08-31 16:38:30 -0400

    styling and procfile

---
+++ public/js/script.js
@@ -0,0 +1,24 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const express = require("express");
 const MenuItem = require("./models/menuItems");
 const CartItem = require("./models/cart");
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const methodOverride = require("method-override");
 
 // SETUP MONGOOSE
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/shop");
+const MONGODB_URI =
+  "mongodb+srv://usmanaqadri:shuraim@restaurant.44woy1y.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 mongoose.connection.once("open", () => {
   console.log("connected to mongo");
 });
@@ -25,39 +30,80 @@ app.get("/", (req, res) => {
 
 // SEED ROUTE
 
-app.get("/seed", async (req, res) => {
-  const newProducts = [
-    {
-      name: "Beans",
-      desc: "A small pile of beans. Buy more beans for a big pile of beans.",
-      imgUrl:
-        "https://cdn3.bigcommerce.com/s-a6pgxdjc7w/products/1075/images/967/416130__50605.1467418920.1280.1280.jpg?c=2",
-      price: 5,
-      rating: 5,
-    },
-    {
-      name: "Bones",
-      desc: "It's just a bag of bones.",
-      imgUrl: "http://bluelips.com/prod_images_large/bones1.jpg",
-      price: 25,
-      rating: 5,
-    },
-    {
-      name: "Bins",
-      desc: "A stack of colorful bins for your beans and bones.",
-      imgUrl: "http://www.clipartbest.com/cliparts/9cz/rMM/9czrMMBcE.jpeg",
-      price: 7000,
-      rating: 5,
-    },
-  ];
+// app.get("/seed", async (req, res) => {
+//   const newProducts = [
+//     {
+//       name: "Double Cheeseburger",
+//       desc: "The Double Cheeseburger features two 100% pure all beef patties seasoned with just a pinch of salt and pepper. It's topped with tangy pickles, chopped onions, ketchup, mustard, and two melty American cheese slices.",
+//       imgUrl:
+//         "https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-Double-Cheeseburger-1:1-3-product-tile-desktop?wid=829&hei=515&dpr=off",
+//       price: 6,
+//       rating: 4.5,
+//     },
+//     {
+//       name: "Philly Cheesesteak",
+//       desc: "A sandwich made with super thinly sliced ribeye steak, caramelized onion, and provolone cheese. That simple combination is the original classic as made popular on the East Coast.",
+//       imgUrl: "https://tipbuzz.com/wp-content/uploads/Philly-Cheesesteak-1.jpg",
+//       price: 8,
+//       rating: 4.5,
+//     },
+//     {
+//       name: "Sesame Chicken",
+//       desc: "A dish commonly found in Chinese restaurants throughout the United States and Canada and is similar to General Tso's chicken but the taste is sweet rather than spicy.",
+//       imgUrl:
+//         "https://www.dinneratthezoo.com/wp-content/uploads/2015/04/sesame-chicken-1.jpg",
+//       price: 12,
+//       rating: 5.5,
+//     },
+//     {
+//       name: "Spicy Fried Chicken Sandwich",
+//       desc: "This fried chicken sandwich is ultra crispy on the outside, tender & juicy on the inside and bursting with flavour all over!.",
+//       imgUrl:
+//         "https://somethingaboutsandwiches.com/wp-content/uploads/2021/03/crispy-chicken-sandwich.jpg",
+//       price: 7,
+//       rating: 5,
+//     },
+//     {
+//       name: "Deep Dish Pizza",
+//       desc: "a thick pizza baked in a pan and layered with cheese, fillings like meat and vegetables, and sauce–in that order.",
+//       imgUrl:
+//         "https://www.thespruceeats.com/thmb/Azfb6CWxzCNTPswGrUDYUEvrAGU=/940x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/chicago-deep-dish-pizza-pie--141427346-5997aad0af5d3a0011164ed8.jpg",
+//       price: 16,
+//       rating: 5,
+//     },
+//     {
+//       name: "Butter Chicken",
+//       desc: "A classic Indian dish where grilled chicken is simmered in a spicy, aromatic, buttery and creamy tomato gravy.",
+//       imgUrl:
+//         "https://cafedelites.com/wp-content/uploads/2019/01/Butter-Chicken-IMAGE-27.jpg",
+//       price: 17,
+//       rating: 4.5,
+//     },
+//     {
+//       name: "Lamb Gyro Platter",
+//       desc: "Lamb gyros meat served with a small salad and your choice or rice or fries, and a side of tzatiki.",
+//       imgUrl:
+//         "https://media-cdn.tripadvisor.com/media/photo-s/10/d6/03/a2/lamb-gyro-platter.jpg",
+//       price: 11,
+//       rating: 4.5,
+//     },
+//     {
+//       name: "Chicken Shawarma Wrap",
+//       desc: "Authentic Chicken Shawarma is cooked by stacking spice-marinated chicken on a vertical spit, like a roaster.",
+//       imgUrl:
+//         "https://www.lemonblossoms.com/wp-content/uploads/2020/06/Chicken-Shawarma-Recipe-14.jpg",
+//       price: 7,
+//       rating: 5,
+//     },
+//   ];
 
-  try {
-    const seedItems = await MenuItem.create(newProducts);
-    res.send(seedItems);
-  } catch (err) {
-    res.send(err.message);
-  }
-});
+//   try {
+//     const seedItems = await MenuItem.create(newProducts);
+//     res.send(seedItems);
+//   } catch (err) {
+//     res.send(err.message);
+//   }
+// });
 
 // INDEX ROUTE
 
@@ -77,8 +123,6 @@ app.get("/eatHub", (req, res) => {
   });
 });
 
-app.get("eatHub/order");
-
 //  NEW ROUTE
 
 app.get("/eatHub/order", (req, res) => {
@@ -86,7 +130,13 @@ app.get("/eatHub/order", (req, res) => {
     if (err) {
       console.log("error", err);
     } else {
-      res.render("order.ejs", { menu });
+      CartItem.find({}, (err, cart) => {
+        if (err) {
+          console.log("error", err);
+        } else {
+          res.render("order.ejs", { menu, cart });
+        }
+      });
     }
   });
 });
@@ -181,4 +231,4 @@ app.delete("/eatHub/order/:id", (req, res) => {
   });
 });
 
-app.listen(port, () => console.log(`proj 2 on http://localhost:${port}`));
+app.listen(PORT, () => console.log(`proj 2 on http://localhost:${PORT}`));
