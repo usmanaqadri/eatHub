60edbb66972b6b79b84addc88ae2fa12187b2b91
usmanaqadri <usmanaqadri@gmail.com>
2022-09-01 08:54:16 -0400

    added controller & refactoring server.js

---
+++ controllers/cart-control.js
@@ -0,0 +1,137 @@
+const express = require("express");
+const router = express.Router();
+const MenuItem = require("../models/menuItems");
+const CartItem = require("../models/cart");
+
+// INDEX ROUTE
+
+router.get("/", (req, res) => {
+  MenuItem.find({}, (err, items) => {
+    if (err) {
+      console.log("error", err);
+    } else {
+      CartItem.find({}, (err, cart) => {
+        if (err) {
+          console.log("error", err);
+        } else {
+          res.render("index.ejs", { items, cart });
+        }
+      });
+    }
+  });
+});
+
+// SUBMIT ROUTE
+router.get("/submit", (req, res) => {
+  res.render("submit.ejs");
+});
+
+//  NEW ROUTE
+
+router.get("/order", (req, res) => {
+  MenuItem.find({}, (err, menu) => {
+    if (err) {
+      console.log("error", err);
+    } else {
+      CartItem.find({}, (err, cart) => {
+        if (err) {
+          console.log("error", err);
+        } else {
+          res.render("order.ejs", { menu, cart });
+        }
+      });
+    }
+  });
+});
+
+// SHOW ROUTE
+
+router.get("/:id", (req, res) => {
+  MenuItem.findById(req.params.id, (err, item) => {
+    if (err) {
+      console.log("error", err);
+    } else {
+      res.render("show.ejs", { item });
+    }
+  });
+});
+
+// CREATE ROUTE
+router.post("/", ({ body }, res) => {
+  if (Object.keys(body).length === 0) {
+    res.redirect("/eatHub");
+  } else {
+    let { order } = body;
+    if (typeof order === "string") {
+      order = [order];
+    }
+    const newCartItems = [];
+    for (let eachItem of order) {
+      const item = { name: "", price: null, qty: null };
+      eachItem = eachItem.split(";");
+      item.name = eachItem[0];
+      item.price = Number(eachItem[1]);
+      item.qty = 1;
+      newCartItems.push(item);
+    }
+    CartItem.create(newCartItems, (err, data) => {
+      if (err) {
+        console.log("error", err);
+      } else {
+        res.redirect("/eatHub");
+      }
+    });
+  }
+});
+
+// EDIT ROUTE
+router.get("/order/:id/edit", (req, res) => {
+  CartItem.findById(req.params.id, (err, item) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("edit.ejs", { item });
+    }
+  });
+});
+
+// UPDATE ROUTE
+router.put("/", (req, res) => {
+  const update = Object.entries(req.body)[0];
+  CartItem.findByIdAndUpdate(
+    update[0],
+    { qty: update[1] },
+    { new: true },
+    (err, updatedItem) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.redirect("/eatHub");
+      }
+    }
+  );
+});
+
+// DESTROY ROUTES
+
+router.delete("/", (req, res) => {
+  CartItem.deleteMany({}, (err, cart) => {
+    if (err) {
+      console.log("error", err);
+    } else {
+      res.redirect("/eatHub");
+    }
+  });
+});
+
+router.delete("/order/:id", (req, res) => {
+  CartItem.findByIdAndRemove(req.params.id, (err, deletedItem) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/eatHub");
+    }
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const express = require("express");
-const MenuItem = require("./models/menuItems");
-const CartItem = require("./models/cart");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const methodOverride = require("method-override");
+
+const eatHubController = require("./controllers/cart-control");
 
 // SETUP MONGOOSE
 const mongoose = require("mongoose");
@@ -23,6 +23,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 
+app.use("/eatHub", eatHubController);
+
+// DEFAULT
 app.get("/", (req, res) => {
   res.send("you tracking?");
 });
@@ -103,136 +106,5 @@ app.get("/", (req, res) => {
 //     res.send(err.message);
 //   }
 // });
-
-// INDEX ROUTE
-
-app.get("/eatHub", (req, res) => {
-  MenuItem.find({}, (err, items) => {
-    if (err) {
-      console.log("error", err);
-    } else {
-      CartItem.find({}, (err, cart) => {
-        if (err) {
-          console.log("error", err);
-        } else {
-          res.render("index.ejs", { items, cart });
-        }
-      });
-    }
-  });
-});
-
-// SUBMIT ROUTE
-app.get("/eatHub/submit", (req, res) => {
-  res.render("submit.ejs");
-});
-
-//  NEW ROUTE
-
-app.get("/eatHub/order", (req, res) => {
-  MenuItem.find({}, (err, menu) => {
-    if (err) {
-      console.log("error", err);
-    } else {
-      CartItem.find({}, (err, cart) => {
-        if (err) {
-          console.log("error", err);
-        } else {
-          res.render("order.ejs", { menu, cart });
-        }
-      });
-    }
-  });
-});
-
-// SHOW ROUTE
-
-app.get("/eatHub/:id", (req, res) => {
-  MenuItem.findById(req.params.id, (err, item) => {
-    if (err) {
-      console.log("error", err);
-    } else {
-      res.render("show.ejs", { item });
-    }
-  });
-});
-
-// CREATE ROUTE
-app.post("/eatHub", ({ body }, res) => {
-  if (Object.keys(body).length === 0) {
-    res.redirect("/eatHub");
-  } else {
-    let { order } = body;
-    if (typeof order === "string") {
-      order = [order];
-    }
-    const newCartItems = [];
-    for (let eachItem of order) {
-      const item = { name: "", price: null, qty: null };
-      eachItem = eachItem.split(";");
-      item.name = eachItem[0];
-      item.price = Number(eachItem[1]);
-      item.qty = 1;
-      newCartItems.push(item);
-    }
-    CartItem.create(newCartItems, (err, data) => {
-      if (err) {
-        console.log("error", err);
-      } else {
-        res.redirect("/eatHub");
-      }
-    });
-  }
-});
-
-// EDIT ROUTE
-app.get("/eatHub/order/:id/edit", (req, res) => {
-  CartItem.findById(req.params.id, (err, item) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("edit.ejs", { item });
-    }
-  });
-});
-
-// UPDATE ROUTE
-app.put("/eatHub", (req, res) => {
-  const update = Object.entries(req.body)[0];
-  CartItem.findByIdAndUpdate(
-    update[0],
-    { qty: update[1] },
-    { new: true },
-    (err, updatedItem) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.redirect("/eatHub");
-      }
-    }
-  );
-});
-
-// DESTROY ROUTES
-
-app.delete("/eatHub", (req, res) => {
-  CartItem.deleteMany({}, (err, cart) => {
-    if (err) {
-      console.log("error", err);
-    } else {
-      res.redirect("/eatHub");
-    }
-  });
-});
-
-app.delete("/eatHub/order/:id", (req, res) => {
-  CartItem.findByIdAndRemove(req.params.id, (err, deletedItem) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.redirect("/eatHub");
-    }
-  });
-});
 
 app.listen(PORT, () => console.log(`proj 2 on http://localhost:${PORT}`));
